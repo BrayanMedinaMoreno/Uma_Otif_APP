27cc0cfcfc348fcdba3dd21a630afedcdaaf5924
BrayanMedinaMoreno <brayan0519@yahoo.com>
2023-12-11 16:03:06 -0500

    en aun no terminada

--- funcionalidades/crear_otif.py
+++ funcionalidades/crear_otif.py
@@ -11,9 +11,24 @@ def creacion_otif(otif_create_file):
         print(datos_a_llenar.head())
 
         print("Leyendo plantilla...")
-        
+        plantilla = pd.read_excel("/plantilla/otif.xlsx")
+        print(plantilla)
 
+
+        print("Llenando datos...")
+        for index, row in datos_a_llenar.iterrows():
+            plantilla.loc[index, "Documento compras"] = row["Documento compras"]
+            plantilla.loc[index, "Proveedor/Centro suministrador"] = row["Proveedor/Centro suministrador"]
+            plantilla.loc[index, "fecha entrega CKD"] = row["Fecha documento"]
+            plantilla.loc[index, "Material"] = row["Material"]
+            plantilla.loc[index, "Texto breve"] = row["Texto breve"]
+            plantilla.loc[index, "Precio neto"] = row["Precio neto"]
+            plantilla.loc[index, "Cantidad de pedido"] = row["Cantidad de pedido"]
+            plantilla.loc[index, "Cantidad de reparto"] = row["Cantidad de reparto"]  
+
+        file_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=)
 
     
     except Exception as e:
         messagebox.showerror("Error", str(e))
+
