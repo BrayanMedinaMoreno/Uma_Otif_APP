004746af5a29273dc9616f1d0c33a3b235cdf57d
BrayanMedinaMoreno <brayan0519@yahoo.com>
2023-12-11 10:28:58 -0500

    ventana principal tk

--- app.py
+++ app.py
@@ -5,11 +5,29 @@ import pandas as pd
 
 export_file = ""
 otif_file = ""
-otif_create file = ""
+otif_create_file = ""
 otiftime_file = ""
-otif_grafico = ""
 mb51 = ""
 otif_entradas = ""
 
 ### Manejo // Ventanas // FunOcultar ###
 
+def abrir_principal():
+        home.withdraw()
+
+
+### Interfaz // Inicio ###
+
+home = Tk()
+home.title("OTIF APP")
+home.geometry("800x600")
+home.resizable(False,False)
+
+canvas = Canvas(home,width=800, height=600)
+canvas.pack()
+
+
+canvas.create_rectangle(0,0,800,90, fill="black")
+
+
+home.mainloop()
