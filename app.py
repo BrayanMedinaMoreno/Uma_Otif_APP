5236ff3abf109883e5f47de39bb8f39c5d5a2b0f
BrayanMedinaMoreno <brayan0519@yahoo.com>
2023-12-13 16:58:50 -0500

    si

--- app.py
+++ app.py
@@ -27,6 +27,6 @@ etiquita_texto_logo.place(relx=0.5, rely=0.02, anchor="n")
 boton_mb51 = Button(home, text="Seleccionar archivo export", bg="#fafafc", command=carga_archivo_export)
 boton_generar_otif = Button(home, text="Generar", bg="#fafafc", command=combinar_archivos)
 
-boton_mb51.place(relx=0.20, rely=0.5, anchor="center")
-boton_generar_otif.place(relx=0.20, rely=0.7, anchor="center")
+boton_mb51.place(relx=0.15, rely=0.23, anchor="center")
+boton_generar_otif.place(relx=0.15, rely=0.29, anchor="center")
 home.mainloop()
