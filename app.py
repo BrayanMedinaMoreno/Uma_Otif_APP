d7021a648b5c101065aef98dea9d3909e61c3a4e
BrayanMedinaMoreno <brayan0519@yahoo.com>
2024-01-09 16:59:18 -0500

    new buttons

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from tkinter import *
 import pandas as pd 
 from funcionalidades.botones import carga_archivo_export,combinar_archivos, abrir_otiftime, combinar_otiftime
 
-
 ### Interfaz // Inicio ###
 home = Tk()
 home.title("OTIF APP")
@@ -14,7 +13,6 @@ canvas.pack()
 
 # Parte superior negra #
 canvas.create_rectangle(0,0,800,90, fill="black")
-canvas.create_line(0,0,20,20, fill="white")
 
 # logo # 
 logo = PhotoImage(file="Logos/uma-logo.png")
@@ -25,11 +23,11 @@ color_logo.place(relx=0.5, rely=0.04, anchor="n")
 etiquita_texto_logo = Label(home, text="G    R    U    P    O", bg="black", fg="white")
 etiquita_texto_logo.place(relx=0.5, rely=0.02, anchor="n")
 
-# texto sobre los botones #
-# crear otif#
+## texto sobre los botones ##
+# crear otif #
 etiquita_texto_botones_crear_otif = Label(home, text="Crear Otif Primer Paso", fg="black")
-etiquita_texto_botones_crear_otif.place(relx=0.15, rely=0.19, anchor="center")
-# Texto sobre los botones fechas#
+etiquita_texto_botones_crear_otif.place(relx=0.20, rely=0.19, anchor="center")
+# Texto sobre los botones fechas #
 etiquita_texto_boton_fechas = Label(home, text="Calcular las fechas", fg="black" ) 
 etiquita_texto_boton_fechas.place(relx=0.80, rely=0.19, anchor="center")
 
@@ -37,18 +35,19 @@ etiquita_texto_boton_fechas.place(relx=0.80, rely=0.19, anchor="center")
 # crear otif #
 boton_export = Button(home, text="Seleccionar archivo export", bg="#fafafc", command=carga_archivo_export)
 boton_generar_otif = Button(home, text="Generar", bg="#fafafc", command=combinar_archivos)
-boton_export.place(relx=0.15, rely=0.23, anchor="center")
-boton_generar_otif.place(relx=0.15, rely=0.29, anchor="center")
+boton_export.place(relx=0.20, rely=0.23, anchor="center")
+boton_generar_otif.place(relx=0.20, rely=0.29, anchor="center")
 
 # fechas #
-boton_fechas = Button(home, text="Seleccionar archivo para calcular fechas", bg="#fafafc", command=abrir_otiftime)
+boton_fechas = Button(home, text="Seleccionar archivo fechas", bg="#fafafc", command=abrir_otiftime)
 boton_generar_fechas = Button(home, text="Calcular Fechas", bg="#fafafc", command=combinar_otiftime)
 boton_fechas.place(relx=0.80, rely=0.23, anchor="center")
 boton_generar_fechas.place(relx=0.80, rely=0.29, anchor="center")
 
 # update otif #
-boton_mb51 = Button(home, text="Seleccionar archivo mb51",fg="black")
-boton_otif = Button(home, text="Seleccionar archivo otif",fg="black")
-
+boton_mb51 = Button(home, text="Seleccionar archivo mb51",bg="#fafafc")
+boton_otif = Button(home, text="Seleccionar archivo otif",bg="#fafafc")
+boton_mb51.place(relx=0.50, rely=0.23, anchor="center")
+boton_otif.place(relx=0.50, rely=0.29, anchor="center")
 
 home.mainloop()
